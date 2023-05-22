5b99a6b2c6666c187b584341c86ddaab33fded26
Vu Van Dung <me@joulev.dev>
2023-05-22 14:13:56 +0700

    improve styling of Question component
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/[id]/components.tsx
+++ app/[id]/components.tsx
@@ -22,9 +22,17 @@ export function code({ children, ...rest }: React.ComponentProps<"code">) {
 
 async function __Question({ id }: { id: string }) {
   const title = await getTitle(id);
+  // We need to ensure the help-circle stays on the same line as the link, while
+  // the underline is displayed nicely, the spacing are good and if the help-circle
+  // is at the start of a line, it aligns well with the left margin of the whole
+  // text. See also https://stackoverflow.com/a/75070847
   return (
     <>
-      <HelpCircle size="18" className="inline-block text-daw-slate-600 mx-1" />
+      &nbsp;
+      <span className="whitespace-nowrap">
+        <HelpCircle size="18" className="inline-block text-daw-slate-600 mr-1" />
+        &nbsp;
+      </span>
       <A href={`/${id}`}>{title}</A>
     </>
   );
