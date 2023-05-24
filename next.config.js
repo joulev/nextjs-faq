0927e09c2c105717d64801946ee2d07aef68c7a7
Vu Van Dung <me@joulev.dev>
2023-05-24 09:50:56 +0700

    remove pages which are now in the official docs
    Signed-off-by: Vu Van Dung <me@joulev.dev>

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import("next").NextConfig} */
+module.exports = {
+  redirects: async () => [
+    {
+      source: "/add-headers-and-status-to-response-in-route-handlers",
+      destination:
+        "https://nextjs.org/docs/app/building-your-application/routing/router-handlers#cors",
+      permanent: true,
+    },
+    {
+      source: "/request-body-in-route-handlers",
+      destination:
+        "https://nextjs.org/docs/app/building-your-application/routing/router-handlers#request-body",
+      permanent: true,
+    },
+  ],
+};
