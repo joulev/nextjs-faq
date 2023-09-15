2cb748ee9de67f478136f7e94628148fec4564a6
Vu Van Dung <me@joulev.dev>
2023-09-15 08:21:42 +0700

    add authors field to pages
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -9,6 +9,12 @@ export default makeSource(
         description: "Last updated date of the post",
         required: true,
       },
+      authors: {
+        type: "list",
+        of: { type: "string" },
+        description: "The authors of the post (GitHub usernames list without the @)",
+        required: true,
+      },
     },
   }),
 );
